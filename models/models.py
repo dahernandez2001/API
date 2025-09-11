e4d013b8b39ca588a97d96073a4b02d57c1e7c5a
dahernandez2001 <147889856+dahernandez2001@users.noreply.github.com>
2025-09-11 01:19:10 +0000

    quinto

--- models/models.py
+++ models/models.py
@@ -9,3 +9,21 @@ class Director(db.Model):
 
     def to_dict(self):
         return {"id": self.id, "name": self.name}
+
+from app import db
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    director_id = db.Column(db.Integer, db.ForeignKey("directors.id"), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "year": self.year,
+            "director_id": self.director_id,
+            "director_name": self.director.name if self.director else None,
+        }
