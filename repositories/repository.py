1c3c96c275f64c6192e1d04a44de3c23c38af081
dahernandez2001 <147889856+dahernandez2001@users.noreply.github.com>
2025-09-11 01:21:42 +0000

    septimoo

--- repositories/repository.py
+++ repositories/repository.py
@@ -27,3 +27,38 @@ class DirectorRepository:
             db.session.delete(director)
             db.session.commit()
         return director
+
+from models.movie import Movie
+from app import db
+
+class MovieRepository:
+    def get_all(self):
+        return Movie.query.all()
+
+    def get_by_id(self, id_):
+        return Movie.query.get(id_)
+
+    def create(self, title, year, director_id):
+        movie = Movie(title=title, year=year, director_id=director_id)
+        db.session.add(movie)
+        db.session.commit()
+        return movie
+
+    def update(self, id_, title=None, year=None, director_id=None):
+        movie = self.get_by_id(id_)
+        if movie:
+            if title:
+                movie.title = title
+            if year:
+                movie.year = year
+            if director_id:
+                movie.director_id = director_id
+            db.session.commit()
+        return movie
+
+    def delete(self, id_):
+        movie = self.get_by_id(id_)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
+        return movie
